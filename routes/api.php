dbad10cb855746f7a186026da66a277a87631054
Stephano Ramos <stephano.ramos@uber.com>
2023-05-03 18:39:28 -0300

    removes passport

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,5 @@
 <?php
 
-use App\Http\Controllers\DriverController;
-use App\Http\Controllers\GoogleMapsController;
-use App\Http\Controllers\ItineraryController;
-use App\Http\Controllers\ResponsibleController;
-use App\Http\Controllers\SchoolController;
-use App\Http\Controllers\StudentController;
-use App\Http\Controllers\UserController;
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -19,17 +10,3 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware(['verified', 'auth:sanctum'])->group(function () {
-    Route::get('/user/me', [UserController::class, 'me']);
-    Route::post('/user/logout', [UserController::class, 'logout']);
-    Route::get('/maps/autocomplete', [GoogleMapsController::class, 'autocomplete']);
-    Route::apiResource('itinerary', ItineraryController::class);
-    Route::apiResource('driver', DriverController::class)->only('index', 'store', 'show');
-    Route::apiResource('responsible', ResponsibleController::class)->only('index', 'store', 'show');
-    Route::apiResource('school', SchoolController::class)->only('index', 'store', 'show', 'update');
-    Route::apiResource('student', StudentController::class);
-    Route::apiResource('user', UserController::class);
-    Route::apiResource('user.driver', DriverController::class)->only('update', 'destroy');
-    Route::apiResource('user.responsible', ResponsibleController::class)->only('update', 'destroy');
-});
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,12 @@
 <?php
 
+use App\Http\Controllers\DriverController;
+use App\Http\Controllers\GoogleMapsController;
+use App\Http\Controllers\ItineraryController;
+use App\Http\Controllers\ResponsibleController;
+use App\Http\Controllers\SchoolController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +22,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
+});
+
+Route::middleware(['verified', 'auth:sanctum'])->group(function () {
+    Route::get('/user/me', [UserController::class, 'me']);
+    Route::post('/user/logout', [UserController::class, 'logout']);
+    Route::get('/maps/autocomplete', [GoogleMapsController::class, 'autocomplete']);
+    Route::apiResource('itinerary', ItineraryController::class);
+    Route::apiResource('driver', DriverController::class)->only('index', 'store', 'show');
+    Route::apiResource('responsible', ResponsibleController::class)->only('index', 'store', 'show');
+    Route::apiResource('school', SchoolController::class)->only('index', 'store', 'show', 'update');
+    Route::apiResource('student', StudentController::class);
+    Route::apiResource('user', UserController::class);
+    Route::apiResource('user.driver', DriverController::class)->only('update', 'destroy');
+    Route::apiResource('user.responsible', ResponsibleController::class)->only('update', 'destroy');
 });
 
 require __DIR__.'/auth.php';
